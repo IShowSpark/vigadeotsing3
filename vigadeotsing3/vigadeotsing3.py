e9a61abcf2b6ebab48ccfbe8c4c60d21d7dc0de5
opilane <opilane@HP-CZC6297BB7>
2022-01-03 10:17:17 +0200

    poisk_osibok

--- vigadeotsing3/vigadeotsing3.py
+++ vigadeotsing3/vigadeotsing3.py
@@ -28,17 +28,34 @@ def arvud_loendis():
         print(s)
 
 def vahetus(a:int,b:int):
+    '''Kui a>b siis vahetame neid
+    :param int a: Arv, mis on suurem kui b
+    :param int b: Arv, mis on väiksem kui a
+    :rtype:int,int
+    '''
     abi=a
     a=b
     b=abi
     return a,b
 
-def generaator(n,loend,a,b):
+def generaator(n:int,loend:list,a:int,b:int):
+    '''Kui a>b siis vahetame neid
+    :param int n: skolko 4icel
+    :param list loend: 4itaet i zanoscit v spisok 4icla
+    :param int a: minimalnoe 4iclo
+    :param int b: maksimalnoe 4iclo
+    '''
     for i in range (n):
         loend.append(randint(a,b))
     
 
-def jagamine(loend,p,n,nol):
+def jagamine(loend:list,p:int,n:int,nol:int):
+    '''
+    :param list loend: 4itaet i zanoscit v spisok 4icla
+    :param int p: polozitelnie 
+    :param int n: otricatelnie
+    :param int nol: nulevie
+    '''
     for el in loend:
         if el>0:
             p(append(pos))
@@ -47,7 +64,10 @@ def jagamine(loend,p,n,nol):
         else:
             nol(append(nol))
 
-def keskmine(loend):
+def keskmine(loend:list):
+    '''
+    :param list loend: 4itaet i zanoscit v spisok 4icla
+    '''
     n=len(loend)
     if n==0:
         kesk=0
@@ -58,7 +78,11 @@ def keskmine(loend):
         kesk=round(sum/n,2)
     return kesk
 
-def lisamine(loend,el):
+def lisamine(loend:list,el:int):
+    '''
+    :param list loend: 4itaet i zanoscit v spisok 4icla
+    :param int el:
+    '''
     loend(append(el))
     loend(sort)
 
